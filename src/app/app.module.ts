3f620bdd7af4fd6dec18bf4bf46f7550e7789c81
josephiznot <josephiznot@gmail.com>
2018-08-21 06:35:03 -0500

    Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavBarComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, NgModule } from "@angular/core";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+@Component({
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.scss"]
+})
+export class NavBarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
