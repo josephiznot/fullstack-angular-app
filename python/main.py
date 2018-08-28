fdc2b38df84846dceb6db179d2b1142e8de20d70
josephiznot <josephiznot@gmail.com>
2018-08-28 15:30:49 -0500

    auto save

--- python/main.py
+++ python/main.py
@@ -1,31 +1,49 @@
 import webapp2
+import logging
 from google.appengine.ext import ndb
-
-keys=[]
-
 class Task(ndb.Model):
     task = ndb.StringProperty()
-    description = ndb.StringProperty()
     isComplete = ndb.BooleanProperty() 
 
-class MainPage(webapp2.RequestHandler):
+#REQUEST HANDLERS
+class GetTasks(webapp2.RequestHandler):
     def get(self):
-        task = ndb.Key('Task', 5733403383037952)
-        tasks= self.request.get('Task')
+        print('---------------PARAMS-------------', self.request.params)
+        # self.request.params["task"] != ''
+        response = Task.query().fetch()
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(tasks)
-
+        self.response.write(response)
+class GetTask(webapp2.RequestHandler):
+    def get(self):
+        try
+class NewTask(webapp2.RequestHandler):
     def post(self):
-        print("TASKKKKKKKKKKKKK")
-        print(ndb)
-        task = Task(task='clean kitchen', 
-                     description="clean counters, scrub floor, load dish washer",
-                     isComplete=False)
-        task_key = task.put()
-        keys.append(task_key)
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(keys)
-
+        print('-----------BODY---------', self.request.POST['task'])
+        clientTask = self.request.POST['task']
+        newTask = Task(task = clientTask, isComplete=False)
+        newTask.put()
+        self.response.headers['Content-Type']="text/plain"
+        self.response.write(newTask)
+        
+#EXCEPTION HANDLERS
+def handle_404(request, response, exception):
+    logging.exception(exception)
+    response.write(exception)
+    response.set_status(404)
+def handle_500(request, response, exception):
+    logging.exception(exception)
+    response.write(exception)
+    response.set_status(500)
+def handle_401(request,response, exception):
+    logging.exception(exception)
+    response.write(exception)
+    response.set_status(401)
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/api/tasks', GetTasks),
+    ('/api/task', GetTask),
+    ('/api/new-task', NewTask)
 ], debug=True)
+
+app.error_handlers[401] = handle_401
+app.error_handlers[404] = handle_404
+app.error_handlers[500] = handle_500
